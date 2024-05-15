614a1ae6be4b3571e5b65e8f83c177821d242fe6
Luis-Adolfo-Araujo <laaan@cesar.school>
2024-05-15 17:56:54 -0300

    started working with jwt to authenticate users sessions

--- app/hello_run.py
+++ app/hello_run.py
@@ -1,19 +1,60 @@
 from app import app
-
 import psycopg2
-from utils import *
-from flask_cors import CORS 
-from flask import Flask, Response, request, jsonify
+from config import *
+from flask_cors import CORS
+import jwt
+from flask import Response, request, jsonify, session, render_template
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
+from datetime import datetime, timedelta
+from functools import wraps
 
 CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 conn_str = f"host={DB_HOST} port={DB_PORT} dbname={DB_NAME} user={DB_USER} password={DB_PASS}"
 
-@app.route("/db", methods=["GET"])
+app.config['SECRET_KEY'] = SECRET_KEY
+
+@app.route("/", methods=["GET"])
 def main():
-    get_db_connection()
-    return ''
+    # if not session.get('logged_in'):
+    return render_template("public/login.html")
+    # else:
+        # return 'user already logged'
+
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        print(token)
+        if not token:
+            return jsonify({'Alert!':'Token missing'})
+        try:
+            return jwt.decode(token, app.config['SECRET_KEY'])
+        except jwt.exceptions.PyJWTError as e:
+            print(f"JWT error: {e}")
+            return None
+    return decorated
+
+@app.route("/home")
+@token_required
+def home():
+    return 'HOME'
+
+@app.route("/login", methods=["POST"])
+def login():
+    try:
+        if request.form['email'] and request.form['password'] == '123':
+            print(request.form['email'])
+            session['logged_in'] = True
+            token = jwt.encode({
+                'email': request.form['email'],
+                'expiration': str(datetime.utcnow() + timedelta(seconds=25))
+            },
+            app.config['SECRET_KEY'])
+            return jsonify({'token': token})
+    except jwt.exceptions.PyJWTError as e:
+        print(f"JWT error: {e}")
+        return None
 
 def get_db_connection():
     try:
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
 
-@app.route("/")
+@app.route("/login")
 def index():
-    return render_template("public/index.html")
+    return render_template("public/login.html")
